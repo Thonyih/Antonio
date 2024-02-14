26a5ea338049bf14c58ae492416ce428e10bd1d3
António Palma <antoniopalma9999@gmail.com>
2024-02-14 09:54:26 +0000

    Small changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 
 
 
+
+function pageChange() {
+    window.location.href = 'templates/myProfile.html';
+}
+
+function goToMainPage() {
+    window.location.href = '../../'; // Adjust the path as needed
+  }
+
+
